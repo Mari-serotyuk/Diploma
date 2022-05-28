4cc48e8aaa51e5dfbef58afedf4fce71d5d4c14d
Mari-serotyuk <99201320+Mari-serotyuk@users.noreply.github.com>
2022-05-28 16:50:30 +0300

    Add files via upload

--- Project №2/script/script.js
+++ Project №2/script/script.js
@@ -1,5 +1,7 @@
-$('.carousel').slick({
-    autoplay: true,
+$('.projects-gallery').slick({
+    autoplay: false,
     autoplaySpeed: 5000,
+    slidesToShow: 4,
+    slidesToScroll: 4,
     dots: true,
 });
