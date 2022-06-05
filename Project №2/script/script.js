a0831f9514d9674493951042629199ede1f4f1ec
Mari-serotyuk <99201320+Mari-serotyuk@users.noreply.github.com>
2022-06-05 10:49:20 +0300

    Add files via upload

--- Project №2/script/script.js
+++ Project №2/script/script.js
@@ -1,7 +1,28 @@
+$('.menu > div').on('click', function(){
+    $(this).next('.click-info').slideToggle(300);
+})
+
+$('#home').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#about').offset().top}, 500);
+})
+
+$('#projects').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#last-projects').offset().top}, 500);
+})
+
 $('.projects-gallery').slick({
     autoplay: false,
     autoplaySpeed: 5000,
     slidesToShow: 4,
     slidesToScroll: 4,
+    dots: true,
+});
+
+$('.carousel').slick({
+    autoplay: false,
+    autoplaySpeed: 5000,
+    slidesToScroll: 1,
     dots: true,
 });
