d06ddb8eaf990ad1297e70ea75c8c0c2a6dfc6c4
Mari-serotyuk <99201320+Mari-serotyuk@users.noreply.github.com>
2022-06-08 16:49:56 +0300

    Add files via upload

--- Project №2/script/script.js
+++ Project №2/script/script.js
@@ -20,12 +20,21 @@ $('.projects-gallery').slick({
     dots: true,
     responsive: [
         {
-            breakpoint: 1090,
+            breakpoint: 1200,
             settings:{
                 slidesToShow: 3,
                 slidesToScroll: 3,
                 infinite: true,
-                dots: true
+                dots: true,
+            }
+        },
+        {
+            breakpoint: 1090,
+            settings:{
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true,
             }
         }
     ]
